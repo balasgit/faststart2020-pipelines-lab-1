6e32bfcc50935d3d3aff64cfd43ece04355b7dda
Bala Sivasubramanian <bala@us.ibm.com>
2020-02-27 12:44:32 -0600

    add contents

--- src/routes/index1.js
+++ src/routes/index1.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SW Brownbag Session 2020' });
+  res.render('index', { title: 'SW Brownbag Session 2020 Feb' });
 });
 
 module.exports = router;
