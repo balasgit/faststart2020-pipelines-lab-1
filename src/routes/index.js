f971a994cd42dc49c451ac759b258a333674eebf
Bala Sivasubramanian <bala@us.ibm.com>
2020-02-28 10:03:35 -0600

    renamed file

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SW Brownbag Session 2020 Feb ' });
+  res.render('index', { title: 'SW Brownbag Session 2020 - 28th Feb' });
 });
 
 module.exports = router;
